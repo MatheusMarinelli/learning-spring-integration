394c180b6a3f738aabca46ef977bd353c672adf9
MatheusMarinelli <matheushlmarinelli@gmail.com>
2022-05-02 20:06:29 -0300

    refactor POC to Integration Flow

--- src/main/java/br/com/poc/spring/integration/config/SpringIntegrationConfig.java
+++ src/main/java/br/com/poc/spring/integration/config/SpringIntegrationConfig.java
@@ -3,29 +3,23 @@ package br.com.poc.spring.integration.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.integration.annotation.InboundChannelAdapter;
-import org.springframework.integration.annotation.Poller;
-import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.config.EnableIntegration;
+import org.springframework.integration.core.GenericSelector;
 import org.springframework.integration.core.MessageSource;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.dsl.Pollers;
 import org.springframework.integration.file.FileReadingMessageSource;
 import org.springframework.integration.file.FileWritingMessageHandler;
-import org.springframework.integration.file.filters.SimplePatternFileListFilter;
-import org.springframework.integration.file.support.FileExistsMode;
 import org.springframework.messaging.MessageHandler;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.util.regex.Pattern;
 
 @Configuration
 @EnableIntegration
 public class SpringIntegrationConfig {
-
-    /*
-    private static final String INPUT_DIR = "E:\\file\\input";
-    private static final String OUTPUT_DIR = "E:\\file\\output";
-    private static final String PROCESSING_DIR = "E:\\file\\processing";
-    private static final String ERROR_DIR = "E:\\file\\error";
-    */
 
     @Value("${file.dir.input}")
     private String inputDir;
@@ -39,71 +33,91 @@ public class SpringIntegrationConfig {
     @Value("${file.dir.error}")
     private String errorDir;
 
-    private static final String FILE_TYPE = "*.txt";
 
-    /**
-     * READ THE SOURCE FILE
-     *
-     * @return
-     */
     @Bean
-    @InboundChannelAdapter(value = "input_channel",poller = @Poller(fixedDelay = "2000"))
-    public MessageSource<File> readFile() {
-        // FILE READER
-        FileReadingMessageSource reader = new FileReadingMessageSource();
-        // SOURCE FILE DIRECTORY
-        reader.setDirectory(new File(inputDir));
-        // SOURCE FILE EXTENSION
-        reader.setFilter(new SimplePatternFileListFilter(FILE_TYPE));
-        return reader;
+    public IntegrationFlow readFileFromInputDirectory() {
+        return IntegrationFlows.from(inputDirectory(), consumer -> consumer.poller(Pollers.fixedDelay(5000)))
+                .filter(fileFilter())
+                .handle(processingDirectoryHandler())
+                .get();
     }
 
     @Bean
-    @InboundChannelAdapter(value = "processing_read_channel",poller = @Poller(fixedDelay = "2000"))
-    public MessageSource<File> readFileProcessing() {
-        // FILE READER
-        FileReadingMessageSource reader = new FileReadingMessageSource();
-        // SOURCE FILE DIRECTORY
-        reader.setDirectory(new File(processingDir));
-        // SOURCE FILE EXTENSION
-        reader.setFilter(new SimplePatternFileListFilter(FILE_TYPE));
-        return reader;
+    public GenericSelector<File> fileFilter() {
+        return new GenericSelector<File>() {
+            @Override
+            public boolean accept(File source) {
+                Pattern p1 = Pattern.compile("ABC.DEF.M[0-9]{3}");
+                Pattern p2 = Pattern.compile("FILE.TEST.H[0-9]{6}");
+                String fileNameWithoutExtension = source.getName().replace(".txt","");
+
+                return (p1.matcher(fileNameWithoutExtension).matches() || p2.matcher(fileNameWithoutExtension).matches())
+                        && source.getName().endsWith(".txt");
+            }
+        };
     }
 
-    /**
-     * WRITTE SOURCE FILE CONTENT IN NEW FILE
-     *
-     * @return
-     */
     @Bean
-    @ServiceActivator(inputChannel = "processing_write_channel")
-    public MessageHandler writtingProcessing() {
+    public MessageSource<File> inputDirectory() {
+        FileReadingMessageSource source = new FileReadingMessageSource();
+        source.setDirectory(new File(inputDir));
+        return source;
+    }
+
+    @Bean
+    public MessageHandler processingDirectoryHandler() {
         FileWritingMessageHandler handler = new FileWritingMessageHandler(new File(processingDir));
-        handler.setFileExistsMode(FileExistsMode.REPLACE);
         handler.setExpectReply(false);
-        // EXCLUDE SOURCE FILE AFTER COPY IT TO THE DESTINATION DIR
         handler.setDeleteSourceFiles(true);
-        return handler;
+        return  handler;
     }
 
-    @Bean
-    @ServiceActivator(inputChannel = "output_channel")
-    public MessageHandler writtingOutput() {
-        FileWritingMessageHandler handler = new FileWritingMessageHandler(new File(outputDir));
-        handler.setFileExistsMode(FileExistsMode.REPLACE);
-        handler.setExpectReply(false);
-        handler.setDeleteSourceFiles(true);
-        return handler;
-    }
-
-    @Bean
-    @ServiceActivator(inputChannel = "error_channel")
-    public MessageHandler writtingError() {
-        FileWritingMessageHandler handler = new FileWritingMessageHandler(new File(errorDir));
-        handler.setFileExistsMode(FileExistsMode.REPLACE);
-        handler.setExpectReply(false);
-        handler.setDeleteSourceFiles(true);
-        return handler;
-    }
+//    @Bean
+//    @InboundChannelAdapter(value = "processing_read_channel",poller = @Poller(fixedDelay = "2000"))
+//    public MessageSource<File> readFileProcessing() {
+//        // FILE READER
+//        FileReadingMessageSource reader = new FileReadingMessageSource();
+//        // SOURCE FILE DIRECTORY
+//        reader.setDirectory(new File(processingDir));
+//        // SOURCE FILE EXTENSION
+//        reader.setFilter(new SimplePatternFileListFilter(FILE_TYPE));
+//        return reader;
+//    }
+//
+//    /**
+//     * WRITTE SOURCE FILE CONTENT IN NEW FILE
+//     *
+//     * @return
+//     */
+//    @Bean
+//    @ServiceActivator(inputChannel = "processing_write_channel")
+//    public MessageHandler writtingProcessing() {
+//        FileWritingMessageHandler handler = new FileWritingMessageHandler(new File(processingDir));
+//        handler.setFileExistsMode(FileExistsMode.REPLACE);
+//        handler.setExpectReply(false);
+//        // EXCLUDE SOURCE FILE AFTER COPY IT TO THE DESTINATION DIR
+//        handler.setDeleteSourceFiles(true);
+//        return handler;
+//    }
+//
+//    @Bean
+//    @ServiceActivator(inputChannel = "output_channel")
+//    public MessageHandler writtingOutput() {
+//        FileWritingMessageHandler handler = new FileWritingMessageHandler(new File(outputDir));
+//        handler.setFileExistsMode(FileExistsMode.REPLACE);
+//        handler.setExpectReply(false);
+//        handler.setDeleteSourceFiles(true);
+//        return handler;
+//    }
+//
+//    @Bean
+//    @ServiceActivator(inputChannel = "error_channel")
+//    public MessageHandler writtingError() {
+//        FileWritingMessageHandler handler = new FileWritingMessageHandler(new File(errorDir));
+//        handler.setFileExistsMode(FileExistsMode.REPLACE);
+//        handler.setExpectReply(false);
+//        handler.setDeleteSourceFiles(true);
+//        return handler;
+//    }
 
 }
